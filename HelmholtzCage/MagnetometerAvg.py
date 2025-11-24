9b02aa1bc827f614aabe586687987a13572c0fbc
JerikoKammerer <jerikokammerer@gmail.com>
2025-11-24 18:44:05 -0500

    added magnometer average

--- HelmholtzCage/MagnetometerAvg.py
+++ HelmholtzCage/MagnetometerAvg.py
@@ -2,6 +2,7 @@ from Sensors import Magnetometer2 as Magnetometer
 from OutputPipeline import Pinout
 import time
 import os
+import sys
 
 mag = Magnetometer.Magnetometer()
 mag.setup()
@@ -10,6 +11,11 @@ mag.setup()
 out_path = os.path.join(os.getcwd(), 'magnetometer_readings.txt')
 
 numReads = 5 #Magnometer reads X times
+if (len(sys.argv) == 2):
+    numReads = int(sys.argv[1])
+xSum = 0
+ySum = 0
+zSum = 0
 
 try:
     with open(out_path, 'w') as f:
@@ -17,10 +23,16 @@ try:
             mag.read()
             f.write("Reading " + str(i+1) + ":\n")
             f.flush() # Ensure data is written to file
-            f.write(str(mag.display('uT')) + '\n')
+            f.write(str(mag.display('G')) + '\n')
+            xSum+=mag.Mx
+            ySum+=mag.My
+            zSum+=mag.Mz
             f.flush() # Ensure data is written to file
             f.write("-----------------------------\n")
             f.flush() # Ensure data is written to file
             time.sleep(1)
+    print("Average X B-field:", (xSum * 10000 / numReads))
+    print("Average Y B-field:", (ySum * 10000 / numReads))
+    print("Average Z B-field:", (zSum * 10000 / numReads))
 except KeyboardInterrupt:
     print("Exiting Program")
